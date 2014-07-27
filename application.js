1cc0ded16c160f85167be543f6e469c06065d39b
indigocode <indigo.developer@gmail.com>
2014-07-27 16:55:30 -0400

    Adding advanced character addition.

--- application.js
+++ application.js
@@ -1,12 +1,5 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var activeLetters = "";
-
-var moveInterval = undefined;
-var updateInterval = undefined;
-
 $(document).ready(function () {
-    moveInterval = setInterval(Game.moveLetters, 100);
-    updateInterval = setInterval(Game.updateLetters, 1000);
+    Game.start();
 });
 
 $(document).keydown(function (e) {
--- game.js
+++ game.js
@@ -1,12 +1,28 @@
 Game = {
-    updateLetters: function () {
-        var letter = this.getRandomLetter();
-        activeLetters = letter + activeLetters;
-        $('#shuttle').text(activeLetters);
+    moveInterval: undefined,
+    updateInterval: undefined,
+    alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+    activeLetters: "",
+
+    start: function () {
+        Game.activeLetters = "";
+        Game.moveInterval = setInterval(Game.moveLetters, 100);
+        Game.updateInterval = setInterval(Game.update, 1000);
+    },
+
+    update: function () {
+        Game.activeLetters = Game.getRandomLetter() + Game.activeLetters;
+        $('#shuttle').text(Game.activeLetters);
+        $('#shuttle').css({left: '0px'});
+        if (Game.activeLetters.length == 10) {
+            Game.end();
+        }
     },
 
     moveLetters: function () {
-
+        $('#shuttle').css({
+            'left': '+=10px'
+        });
     },
 
     evaluateUserInput: function (keyCode) {
@@ -14,7 +30,11 @@ Game = {
     },
 
     getRandomLetter: function () {
-        return alphabet[Math.floor(Math.random() * alphabet.length)];
-    }
+        return Game.alphabet[Math.floor(Math.random() * Game.alphabet.length)];
+    },
 
+    end: function () {
+        clearInterval(Game.moveInterval);
+        clearInterval(Game.updateInterval);
+    }
 };

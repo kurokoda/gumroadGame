72a14b3a882e1e15e98c8f03546f85f6b71cf996
indigocode <indigo.developer@gmail.com>
2014-07-27 14:53:34 -0400

    Adding jquery. Adding base app skeleton html, js, and css

--- application.js
+++ application.js
@@ -1,3 +1,24 @@
-/**
- * Created by Indigo on 7/27/14.
- */
+var moveInterval = undefined;
+var updateInterval = undefined;
+
+$(document).ready(function () {
+    moveInterval = setInterval(moveLetters, 100);
+    updateInterval = setInterval(updateLetters, 1000);
+});
+
+$(document).keydown(function (e) {
+    evaluateUserInput(e.keyCode);
+});
+
+
+function updateLetters(){
+
+}
+
+function moveLetters(){
+
+}
+
+function moveLetters(){
+
+}
